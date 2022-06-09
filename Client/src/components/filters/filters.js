1e9ebf097bf28055692a4c6a27999cbd69ac1169
Albert-25 <ajst_95@outlook.com>
2022-06-09 12:35:10 -0500

    first breakthrough

--- Client/src/components/filters/filters.js
+++ Client/src/components/filters/filters.js
@@ -1,27 +1,21 @@
 const filters = () => {
-    
+
     const $divContainer = document.createElement("div");
     $divContainer.classList.add("container-filter");
     const $orderNameDiv = document.createElement('div');
     $orderNameDiv.classList.add("order-name");
 
-    const handleFilterName = ()=>{
-       alert("handleFilterName")
+    const handleFilterName = () => {
+        alert("handleFilterName")
     }
-
-    $orderNameDiv.innerHTML = `
-    <div>Order Name</div>
-    <select onChange=${()=>handleFilterName()}>
-        <option selected disabled={true}>Select rating</option>
-        <option value="asc">Ascendente</option>
-        <option value="des">Descendente</option>
-    </select>
+    const $selectOrderName = document.createElement("select");
+    $selectOrderName.innerHTML = `
+    <option selected disabled={true}>Select rating</option>
+    <option value="asc">Ascendente</option>
+    <option value="des">Descendente</option>
     `
 
-
-
-
-
+    $orderNameDiv.appendChild($selectOrderName);
     $divContainer.appendChild($orderNameDiv);
     return $divContainer;
 }

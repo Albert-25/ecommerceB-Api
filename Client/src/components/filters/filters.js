d402eb6c20853b4515e98ab0987077a6093ea395
Albert-25 <ajst_95@outlook.com>
2022-06-09 12:10:17 -0500

    first breakthrough

--- Client/src/components/filters/filters.js
+++ Client/src/components/filters/filters.js
@@ -1,21 +1,14 @@
-// const filters = () => {
-//     const $divContainer = document.createElement("div");
-//     const $orderNameDiv = document.createElement('orderNameDiv');
-//     $orderNameDiv.textContent = "Name Order";
-//     $orderNameDiv.classList.add("Name Order")
-//     const $selectOrderName = document.createElement('select')
-//     const 
-//         // < orderNameDiv className = "filter-sort col" >
-//         //                     <orderNameDiv>Order type</orderNameDiv>
-//         //                     <select onChange={handleSort}>
-//         //                         <option selected disabled={true}>Select order</option>
-//         //                         <option value="asc">Ascendant</option>
-//         //                         <option value="des">Descendant</option>
-//         //                     </select>
-//         //                 </orderNameDiv >
+const filters = () => {
+    const $divContainer = document.createElement("div");
+    $divContainer.classList.add("container-filter");
+    var d = "miclase"
+    const $orderNameDiv = document.createElement('orderNameDiv');
+    $orderNameDiv.classList.add("order-name");
+    $divContainer.appendChild($orderNameDiv);
 
-// }
-// export default filters;
-// // git add .
-// // git commit -m "first breakthrough"
-// // git push
+    $orderNameDiv.innerHTML = `<button class =${d}>ORDER NAME</button>`
+}
+export default filters;
+// git add .
+// git commit -m "first breakthrough"
+// git push
